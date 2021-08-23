4715acb8160d0ce3909b75479b52e160f57ada62
Daniel-0510 <63437534+Daniel-0510@users.noreply.github.com>
2021-08-23 23:54:34 +0800

    默认变更列表

---
+++ MusicFront/mymusic/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark">
+          <Header></Header>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <Aside></Aside>
+        </div>
+      </el-col>
+      <el-col :span="20">
+        <div class="grid-content bg-purple-light">
+          <Main></Main>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark">
+          <Footer></Footer>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header/Header";
+import Aside from "@/components/Aside/Aside";
+import Main from "@/components/Main/Main";
+import Footer from "@/components/Footer/Footer";
+
+export default {
+  name: 'App',
+  components: {Header,Aside,Main,Footer}
+}
+</script>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+ul, li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+
+</style>
