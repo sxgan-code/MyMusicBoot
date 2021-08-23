ef1f9cc740a7a4198334449658ac8aff9285bb6d
Daniel-0510 <63437534+Daniel-0510@users.noreply.github.com>
2021-08-23 23:48:08 +0800

    默认变更列表

---
+++ MusicFront/mymusic/src/components/Footer/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="footer-root">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/java/com/daniel/controller/StudentController.java
+++ src/main/java/com/daniel/controller/StudentController.java
@@ -4,8 +4,10 @@ import com.daniel.domain.Student;
 import com.daniel.services.impl.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -16,20 +18,12 @@ public class StudentController {
     @Autowired
     StudentServiceImpl iStudentService;
     @RequestMapping("/test")
-    public ModelAndView test(){
+    @ResponseBody
+    @CrossOrigin(origins = "http://localhost:63342/", maxAge = 3600)
+    public List<Student> test(){
         ModelAndView mv = new ModelAndView();
         List<Student> allStudent = iStudentService.getAllStudent();
-        mv.addObject("allStu", allStudent);
-        mv.setViewName("all");
-        return mv;
-    }
-    @RequestMapping("/testdata")
-    @RequestBody
-    public List testdata(){
-        ModelAndView mv = new ModelAndView();
-        List<Student> allStudent = iStudentService.getAllStudent();
-        mv.addObject("allStu", allStudent);
-        mv.setViewName("all");
         return allStudent;
     }
+    
 }
